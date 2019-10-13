2b7ac895b20bafc5a81076bedd7ec2fa5ba05760
VAnastasya <velcheva.anastasya@yandex.ru>
2019-10-13 21:46:50 +0300

    Update example.cpp

--- examples/winux/example.cpp
+++ examples/winux/example.cpp
@@ -4,6 +4,54 @@
 #include "example1.h"
 #include "CmdLineParser.h"
 
-int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
+using namespace Audioneex;
+
+void PrintUsage()
+{
+    std::cout << "\nUSAGE: example1 [-u <db_url>] [-m <match_type>] "
+                                    "<audio_files_dir>\n\n";
+}
+
+int main(int argc, char** argv) {
+    CmdLineParser cmdLine;
+    CmdLineOptions_t opts;
+    
+    try
+    {
+        cmdLine.Parse(argv, argc, opts);
+
+        AudioIndexingTask itask (opts.apath);
+
+        std::shared_ptr<KVDataStore>
+        dstore ( new DATASTORE_T (opts.db_url) );
+
+        dstore->Open( opts.db_op, true, true );
+
+        std::shared_ptr<Indexer> 
+        indexer ( Indexer::Create() );
+        indexer->SetDataStore( dstore.get() );
+        indexer->SetAudioProvider( &itask );
+        indexer->SetMatchType( opts.mtype );
+
+        itask.SetFID( opts.FID_base );
+        itask.SetDataStore( dstore );
+        itask.SetIndexer( indexer );
+        itask.Run();
+
+        std::cout << "Done" << std::endl;
+    }
+    catch(const bad_cmd_line_exception &ex)
+    {
+        std::cerr << "ERROR: " << ex.what() << std::endl;
+        PrintUsage();
+        return -1;
+    }
+    catch(const std::exception &ex)
+    {
+        std::cerr << "ERROR: " << ex.what() << std::endl;
+        return -1;
+    }    
+        
+    //std::cout << "Hello Easy C++ project!" << std::endl;
+    return 0;    
 }
