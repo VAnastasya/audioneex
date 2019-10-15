77ca7445aecb49efb98ee6786dde45bfc0f0ed09
VAnastasya <velcheva.anastasya@yandex.ru>
2019-10-15 21:03:03 +0300

    Update example3.cpp

--- examples/winux/example3.cpp
+++ examples/winux/example3.cpp
@@ -8,11 +8,6 @@
 */
 
 ///
-/// example3
-///
-/// This example shows how to perform the identification of audio from files.
-/// To run the program, use the following command
-///
 /// example3 [-u <db_url>] [-m <match_type>] [-i <id_type>] [-d <id_mode>] 
 ///          [-b <b_thresh>] [-s <offset>] <audio_path>
 ///
@@ -30,6 +25,7 @@
 #include <memory>
 
 #include "ex_common.h"
+#include "example1.h"
 #include "example3.h"
 #include "CmdLineParser.h"
 
@@ -52,21 +48,30 @@ int main(int argc, char** argv)
     try
     {
         cmdLine.Parse(argv, argc, opts);
-
-        IdentificationTask itask (opts.apath);
-
-        // Create the identification results parser
-        FileIdentificationResultsParser idparser;
+        
+        AudioIndexingTask itask (opts.apath);
+        IdentificationTask rtask (opts.apath);
 
         // Get a connection instance to the datastore
         std::shared_ptr<KVDataStore> 
         dstore ( new DATASTORE_T (opts.db_url) );
-
         dstore->Open( KVDataStore::GET, true, true );
-
+      
+        // Create and set up the indexer      
+        std::shared_ptr<Indexer> indexer ( Indexer::Create() );
+        indexer->SetDataStore( dstore.get() );
+        indexer->SetAudioProvider( &itask );
+        indexer->SetMatchType( opts.mtype );    
+      
+        itask.SetFID( opts.FID_base );
+        itask.SetDataStore( dstore );
+        itask.SetIndexer( indexer );
+        itask.Run();      
+      
+        // Create the identification results parser
+        FileIdentificationResultsParser idparser;
         // Create and set up the recognizer
-        std::shared_ptr<Recognizer> 
-        recognizer ( Recognizer::Create() );
+        std::shared_ptr<Recognizer> recognizer ( Recognizer::Create() );
         recognizer->SetDataStore( dstore.get() );
         recognizer->SetMatchType( opts.mtype );
         recognizer->SetMMS( opts.mms );
@@ -74,22 +79,16 @@ int main(int argc, char** argv)
         recognizer->SetIdentificationMode( opts.id_mode );
         recognizer->SetBinaryIdThreshold( opts.b_thresh );
       
-        /*std::cout<<"mtype="<<opts.mtype<<std::endl;
-        std::cout<<"mms="<<opts.mms<<std::endl;
-        std::cout<<"id_type="<<opts.id_type<<std::endl;
-        std::cout<<"id_mode="<<opts.id_mode<<std::endl;
-        std::cout<<"b_thresh="<<opts.b_thresh<<std::endl;
-        std::cout<<"offset="<<opts.offset<<std::endl;  */    
-
         idparser.SetDatastore( dstore );
         idparser.SetRecognizer( recognizer );
 
-        itask.SetDataStore( dstore );
-        itask.SetRecognizer( recognizer );
-        itask.Connect( &idparser );
-        itask.GetAudioSource()->SetPosition( opts.offset );
-        itask.Run();
+        rtask.SetDataStore( dstore );
+        rtask.SetRecognizer( recognizer );
+        rtask.Connect( &idparser );
+        rtask.GetAudioSource()->SetPosition( opts.offset );
+        rtask.Run();
 
+        dstore->Close();
         std::cout << "Done" << std::endl;
     }
     catch(const bad_cmd_line_exception &ex)
